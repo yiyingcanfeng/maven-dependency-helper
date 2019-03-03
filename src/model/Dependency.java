118046196b6e9693de80bfa3178c781f52cd3718
移影残风 <yiyingcanfeng@qq.com>
2019-03-03 20:46:15 +0800

    init

---
+++ src/model/Dependency.java
@@ -0,0 +1,50 @@
+package model;
+
+public class Dependency {
+    private String groupId;
+    private String artifactId;
+    private String version;
+    private String scope;
+
+    @Override
+    public String toString() {
+        return "Dependency{" +
+                "groupId='" + groupId + '\'' +
+                ", artifactId='" + artifactId + '\'' +
+                ", version='" + version + '\'' +
+                ", scope='" + scope + '\'' +
+                '}';
+    }
+
+    public String getGroupId() {
+        return groupId;
+    }
+
+    public void setGroupId(String groupId) {
+        this.groupId = groupId;
+    }
+
+    public String getArtifactId() {
+        return artifactId;
+    }
+
+    public void setArtifactId(String artifactId) {
+        this.artifactId = artifactId;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
+    public String getScope() {
+        return scope;
+    }
+
+    public void setScope(String scope) {
+        this.scope = scope;
+    }
+}
